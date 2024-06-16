af5da29f755cb8b81dd4e204164ebbc622ae8c18
Fatou <fatoumbacke.diallo@gmail.com>
2024-06-16 21:51:08 +0200

    Mise à jour Mentions légales

--- frontend/src/pages/MentionsLégales.jsx
+++ frontend/src/pages/MentionsLégales.jsx
@@ -1,53 +1,69 @@
 import React from "react";
+import "../styles/MentionsLegales.css";
 
 function MentionsLegales() {
   return (
     <section className="main-mentions">
       <div>
-        <h1>Mentions légales du site www.as-fontenay.fr</h1>
-        <p className="p-mentions">
-          En vigueur au 16/06/2024 Conformément aux dispositions des Articles
-          6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la Confiance
-          dans l&rsquoéconomie numérique, dite L.C.E.N., il est porté à la
-          connaissance des utilisateurs et visiteurs, ci-après «
-          l&rsquoUtilisateur », du site as-fontenay.fr, ci-après le "Site", les
-          présentes mentions légales. La connexion et la navigation sur le Site
-          par l’Utilisateur implique acceptation intégrale et sans réserve des
-          présentes mentions légales. Ces dernières sont accessibles sur le Site
-          à la rubrique « Mentions légales ».
-        </p>
-        <p>
-          ARTICLE 1 - L'EDITEUR L'édition du Site est assurée par FATOU MBACKE
-          DIALLO, Entreprise Individuelle, immatriculée au registre SIRENE de
-          FONTENAY AUX ROSES sous le numéro 75002225300058 dont le siège social
-          est situé au 9 RUE JEAN PIERRE LAURENS, Numéro de téléphone
-          0664959830, Adresse e-mail : contact@fatou-mbacke-diallo.com. Le
-          Directeur de la publication est FATOU MBACKE DIALLO. ci-après
-          l'"Editeur".
-        </p>
-        <p>
-          ARTICLE 2 - L'HEBERGEUR L'hébergeur du Site est la société AWS,
-          filiale du groupe AMAZON.COM, INC dont le siège social est situé au 4
-          PLACE AMEDEE BONNET 69002 LYON, RHONE, avec le lien :
-          https://aws.amazon.com/fr/contact-us/.
-        </p>
-        <p>
-          ARTICLE 3 - ACCES AU SITE Le Site est accessible en tout endroit,
-          7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non
-          et pouvant découlant d’une nécessité de maintenance. En cas de
-          modification, interruption ou suspension du Site, l'Editeur ne saurait
-          être tenu responsable.
-        </p>
-        <p>
-          ARTICLE 4 - COLLECTE DES DONNEES Le site est exempté de déclaration à
-          la Commission Nationale Informatique et Libertés (CNIL) dans la mesure
-          où il ne collecte aucune donnée concernant les utilisateurs. Toute
-          utilisation, reproduction, diffusion, commercialisation, modification
-          de toute ou partie du Site, sans autorisation de l’Editeur est
-          prohibée et pourra entraîner des actions et poursuites judiciaires
-          telles que notamment prévues par le Code de la propriété
-          intellectuelle et le Code civil.
-        </p>
+        <h1 className="h1-mentions">
+          Mentions légales du site : www.as-fontenay.fr
+        </h1>
+        <div>
+          <h2 className="h2-mentions">En vigueur au 16/06/2024</h2>
+          <p className="p-mentions">
+            Conformément aux dispositions des Articles 6-III et 19 de la Loi
+            n°2004-575 du 21 juin 2004 pour la Confiance dans l'économie
+            numérique, dite L.C.E.N., il est porté à la connaissance des
+            utilisateurs et visiteurs, ci-après « l'Utilisateur », du site
+            as-fontenay.fr, ci-après le "Site", les présentes mentions légales.
+            La connexion et la navigation sur le Site par l’Utilisateur implique
+            acceptation intégrale et sans réserve des présentes mentions
+            légales. Ces dernières sont accessibles sur le Site à la rubrique «
+            Mentions légales ».
+          </p>
+        </div>
+        <div>
+          <h2 className="h2-mentions">ARTICLE 1 - L'EDITEUR</h2>
+          <p className="p-mentions">
+            L'édition du Site est assurée par FATOU MBACKE DIALLO, Entreprise
+            Individuelle, immatriculée au registre SIRENE de FONTENAY AUX ROSES
+            sous le numéro 75002225300058 dont le siège social est situé au 9
+            RUE JEAN PIERRE LAURENS, Numéro de téléphone 0664959830, Adresse
+            e-mail : contact@fatou-mbacke-diallo.com. Le Directeur de la
+            publication est FATOU MBACKE DIALLO. ci-après "l'Editeur".
+          </p>
+        </div>
+        <div>
+          <h2 className="h2-mentions">ARTICLE 2 - L'HEBERGEUR</h2>
+          <p className="p-mentions">
+            L'hébergeur du Site est la société AWS, filiale du groupe
+            AMAZON.COM, INC dont le siège social est situé au 4 PLACE AMEDEE
+            BONNET 69002 LYON, RHONE, avec le lien :
+            https://aws.amazon.com/fr/contact-us/.
+          </p>
+        </div>
+        <div>
+          <h2 className="h2-mentions">ARTICLE 3 - ACCES AU SITE</h2>
+          <p className="p-mentions">
+            Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de
+            force majeure, interruption programmée ou non et pouvant découlant
+            d’une nécessité de maintenance. En cas de modification, interruption
+            ou suspension du Site, l'Editeur ne saurait être tenu responsable.
+          </p>
+        </div>
+        <div>
+          <h2 className="h2-mentions">ARTICLE 4 - COLLECTE DES DONNEES</h2>
+          <p className="p-mentions">
+            Le site est exempté de déclaration à la Commission Nationale
+            Informatique et Libertés (CNIL) dans la mesure où il ne collecte
+            aucune donnée concernant les utilisateurs. Toute utilisation,
+            reproduction, diffusion, commercialisation, modification de toute ou
+            partie du Site, sans autorisation de l’Editeur est prohibée et
+            pourra entraîner des actions et poursuites judiciaires telles que
+            notamment prévues par le Code de la propriété intellectuelle et le
+            Code civil.
+          </p>
+        </div>
       </div>
     </section>
   );
